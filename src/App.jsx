2c5e073dac0e9411cc82c9966c729413ea3b27e1
florixak <florixakcz@seznam.cz>
2024-07-23 20:20:34 +0200

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "./components/Header";
+import News from "./components/News";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div className="container">Minecraft Server Website</div>;
+  return (
+    <div className="w-full h-screen">
+      <Header />
+      <News />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
